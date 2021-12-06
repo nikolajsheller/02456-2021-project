c0b2b08dd8b61030e8b294b9eaa2d51e4e93a958
Freja Thoresen <frth@faunaphotonics.com>
2021-12-06 18:10:14 +0100

    Only use times from 8-20.

--- labelled_data/make_data.py
+++ labelled_data/make_data.py
@@ -2,6 +2,7 @@ from tools.constants import *
 from fpmodules import BlobManager
 from tools.generate_data import create_labelled_data
 import fpmodules as fp
+import datetime
 
 mac = 'dca6324634b1'
 
@@ -16,13 +17,15 @@ for date in dates[2:3]:
     print('Number of raw data files:', len(blob_list))
 
     for blob in blob_list:
-
+        dt = blob.split('/')[-1].split('.')[0]
+        hour = datetime.datetime.strptime(dt,'%Y%m%dT%H%M%SZ').hour
+        if hour < 8 or hour > 20:
+            continue
         text_file = os.path.join(RAWDATA_CACHE_PATH, blob.replace('/', '_').split('.')[0] + '.txt')
         # if we ran over the file before, don't do it again. If you want to run it anyway, delete the text file.
         if os.path.exists(text_file) and delete_files:
             continue
 
-        create_labelled_data(blob, chunks=True)
         create_labelled_data(blob, chunks=False)
 
         if delete_files:
